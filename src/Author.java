9063e4913f4d2b00f23e63479592ab38aeb53c55
nastu <nastushik92@bk.ru>
2022-09-12 17:55:44 +0300

    дз правки

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String nameAuthor;
     private String surnameAuthor;
@@ -8,5 +10,25 @@ public class Author {
     }
     public String getSurnameAuthor(){ return this.surnameAuthor;}
 
+    @Override
+    public String toString() {
+        return "Author{" +
+                "nameAuthor='" + nameAuthor + '\'' +
+                ", surnameAuthor='" + surnameAuthor + '\'' +
+                '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author)) return false;
+        Author author = (Author) o;
+        return Objects.equals(nameAuthor, author.nameAuthor) && Objects.equals(getSurnameAuthor(), author.getSurnameAuthor());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameAuthor, getSurnameAuthor());
+    }
+}
 
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.sql.SQLOutput;
 
 public class Main {
@@ -5,34 +7,39 @@ public class Main {
         String nameAuthor = "Michail";
         String nameBook = "Heroy of our time";
         int dateOfReleasing = 1923;
-
-        System.out.println("Имя автора " + nameAuthor + " " + "Название книги " + nameBook + " " + "Дата выпуска " + dateOfReleasing);
-
         String surnameAuthor = "Lermontov";
 
-        System.out.println("Имя автора " + nameAuthor + " " + "Фамилия автора " + surnameAuthor);
 
     }
 
     Book book = new Book("Michail", "Heroy of our time", 1923);
-    System.out.println("book.nameAuthor="+book.getNameAuthor());
-    System.out.println("book.nameBook="+book.getNameBook());
-    System.out.println("book.dateOfReleasing="+book.getDateOfReleasing());
-    book.setDateOfReleasing(1922);
 
-public static void idiot(){
-    String author1 = "Fedor Dostoevsky";
-    String name1 = "Idiot";
-    int date1 = 1965;
-    System.out.println("Имя автора " + author1 + " " + "Название книги " + name1 + " " + "Дата выпуска " + date1);
+    {
 
+        System.out.println("book.nameAuthor=" + book.getNameAuthor());
+        System.out.println("book.nameBook=" + book.getNameBook());
+        System.out.println("book.dateOfReleasing=" + book.getDateOfReleasing());
+        book.setDateOfReleasing(1922);
+    }
 
-    String author2 = "Sergey Esenin";
-    String name2 = "Bereza";
-    int date2= 1952;
-    System.out.println("Имя автора " + author2 + " " + "Название книги " + name2 + " " + "Дата выпуска " + date2);
+    public void idiot() {
+        String author1 = "Fedor Dostoevsky";
+        String name1 = "Idiot";
+        int date1 = 1965;
+        Book idiot = new Book("Fedor Dostoevsky", "Idiot", 1965);
+        System.out.println("book.author1=" + book.getNameAuthor());
+        System.out.println("book.name1=" + book.getNameBook());
+        System.out.println("book.date1=" + book.getDateOfReleasing());
+        book.setDateOfReleasing(1922);}
+
+        String author2 = "Sergey Esenin";
+        String name2 = "Bereza";
+        int date2 = 1952;
+        Book Bereza = new Book("Sergey Esenin", "Beresa", 1952);
+
 }
 
 
 
-}
+
+
